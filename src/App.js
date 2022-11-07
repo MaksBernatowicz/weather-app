5601067441a0fa2e3a4e0a34bca6ee4809daaf52
twój login do GitHuba <96842769+MaksBernatowicz@users.noreply.github.com>
2022-11-07 14:39:58 +0100

    added state for loading with spinner

--- src/App.js
+++ src/App.js
@@ -32,23 +32,54 @@ const App = () => {
   const [data, setData] = useState(null); // state for data
   const [location, setLocation] = useState("Krakow"); // state for location
   const [inputValue, setInputValue] = useState(""); // state for typing name of city or country into input
+  const [animate, setAnimate] = useState(false); // state for shake animation if input for search is empty
+  const [loading, setLoading] = useState(false);
 
   const handleInput = (e) => {
     setInputValue(e.target.value);
   };
 
   const handleSubmit = (e) => {
-    console.log(inputValue);
+    // if input values is not empty
+    if (inputValue !== "") {
+      // set inputValue as current location
+      setLocation(inputValue);
+    }
+
+    // select input
+    const input = document.querySelector("input");
+
+    // if input value is empty
+    if (input.value === "") {
+      // set animate to true
+      setAnimate(true);
+      // after 500 ms set animate to false
+      setTimeout(() => {
+        setAnimate(false);
+      }, 500);
+    }
+
+    // clear input
+    input.value = "";
+
     // prevent defaults
     e.preventDefault();
   };
 
   // fetch the data
   useEffect(() => {
+    // set loading to true
+    setLoading(true);
+
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${APIkey}`;
 
     axios.get(url).then((res) => {
-      setData(res.data);
+      // set the data after 1500 ms
+      setTimeout(() => {
+        setData(res.data);
+        // set loading to false
+        setLoading(false);
+      }, 1500);
     });
   }, [location]);
 
@@ -97,8 +128,10 @@ const App = () => {
     <div className="w-full h-screen bg-gradientBg bg-no-repeat bg-cover bg-center flex flex-col items-center justify-center px-4 lg:px-0">
       {/* form */}
       <form
-        className="h-16 bg-black/30 w-full max-w-[450px] 
-      rounded-full backdrop-blur-[32px] mb-8"
+        className={`${
+          animate ? "animate-shake" : "animate-none"
+        } h-16 bg-black/30 w-full max-w-[450px] 
+      rounded-full backdrop-blur-[32px] mb-8`}
       >
         <div className="h-full relative flex items-center justify-between p-2">
           <input
@@ -117,90 +150,96 @@ const App = () => {
       </form>
       {/* card */}
       <div className="w-full max-w-[450px] bg-black/20 min-h-[584px] text-white backdrop-blur-[32px] rounded-[32px] py-12 px-6">
-        <div>
-          {/* card top */}
-          <div className="flex items-center gap-x-5">
-            {/* icon */}
-            <div className="text-[87px]">{icon}</div>
-            <div>
-              {/* country and city name */}
-              <div className="text-2xl font-semibold">
-                {data.name}, {data.sys.country}
-              </div>
-              {/* date */}
+        {loading ? (
+          <div className="w-full h-full flex justify-center items-center">
+            <ImSpinner8 className="text-white text-5xl animate-spin" />
+          </div>
+        ) : (
+          <div>
+            {/* card top */}
+            <div className="flex items-center gap-x-5">
+              {/* icon */}
+              <div className="text-[87px]">{icon}</div>
               <div>
-                {date.getUTCDate()} / {date.getUTCMonth() + 1} /{" "}
-                {date.getUTCFullYear()}
+                {/* country and city name */}
+                <div className="text-2xl font-semibold">
+                  {data.name}, {data.sys.country}
+                </div>
+                {/* date */}
+                <div>
+                  {date.getUTCDate()} / {date.getUTCMonth() + 1} /{" "}
+                  {date.getUTCFullYear()}
+                </div>
+              </div>
+            </div>
+            {/* card body */}
+            <div className="my-20">
+              <div className="flex justify-center items-center">
+                {/* temperature */}
+                <div className="text-[144px] leading-none font=-light">
+                  {parseInt(data.main.temp)}
+                </div>
+                {/* celsius icon */}
+                <div className="text-4xl">
+                  <TbTemperatureCelsius />
+                </div>
+              </div>
+              {/* weather description */}
+              <div className="capitalize text-center">
+                {data.weather[0].description}
+              </div>
+            </div>
+            {/* card bottom */}
+            <div className="max-w-[378px] mx-auto flex flex-col gap-y-6">
+              <div className="flex justify-between">
+                <div className="flex items-center gap-x-2">
+                  {/* icon */}
+                  <div className="text-[20px]">
+                    <BsEye />
+                  </div>
+                  <div>
+                    Visibility{" "}
+                    <span className="ml-2">{data.visibility / 1000} km</span>
+                  </div>
+                </div>
+                <div className="flex items-center gap-x-2">
+                  {/* icon */}
+                  <div className="text-[20px]">
+                    <BsThermometer />
+                  </div>
+                  <div className="flex">
+                    Feels Like
+                    <span className=" flex ml-2">
+                      {parseInt(data.main.feels_like)}
+                      <TbTemperatureCelsius />
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div className="flex justify-between">
+                <div className="flex items-center gap-x-2">
+                  {/* icon */}
+                  <div className="text-[20px]">
+                    <BsWater />
+                  </div>
+                  <div>
+                    Humidity
+                    <span className="ml-2">{data.main.humidity} %</span>
+                  </div>
+                </div>
+                <div className="flex items-center gap-x-2">
+                  {/* icon */}
+                  <div className="text-[20px]">
+                    <BsWind />
+                  </div>
+                  <div>
+                    Wind <span className="ml-2">{data.wind.speed} m/s</span>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-          {/* card body */}
-          <div className="my-20">
-            <div className="flex justify-center items-center">
-              {/* temperature */}
-              <div className="text-[144px] leading-none font=-light">
-                {parseInt(data.main.temp)}
-              </div>
-              {/* celsius icon */}
-              <div className="text-4xl">
-                <TbTemperatureCelsius />
-              </div>
-            </div>
-            {/* weather description */}
-            <div className="capitalize text-center">
-              {data.weather[0].description}
-            </div>
-          </div>
-          {/* card bottom */}
-          <div className="max-w-[378px] mx-auto flex flex-col gap-y-6">
-            <div className="flex justify-between">
-              <div className="flex items-center gap-x-2">
-                {/* icon */}
-                <div className="text-[20px]">
-                  <BsEye />
-                </div>
-                <div>
-                  Visibility{" "}
-                  <span className="ml-2">{data.visibility / 1000} km</span>
-                </div>
-              </div>
-              <div className="flex items-center gap-x-2">
-                {/* icon */}
-                <div className="text-[20px]">
-                  <BsThermometer />
-                </div>
-                <div className="flex">
-                  Feels Like
-                  <span className=" flex ml-2">
-                    {parseInt(data.main.feels_like)}
-                    <TbTemperatureCelsius />
-                  </span>
-                </div>
-              </div>
-            </div>
-            <div className="flex justify-between">
-              <div className="flex items-center gap-x-2">
-                {/* icon */}
-                <div className="text-[20px]">
-                  <BsWater />
-                </div>
-                <div>
-                  Humidity
-                  <span className="ml-2">{data.main.humidity} %</span>
-                </div>
-              </div>
-              <div className="flex items-center gap-x-2">
-                {/* icon */}
-                <div className="text-[20px]">
-                  <BsWind />
-                </div>
-                <div>
-                  Wind <span className="ml-2">{data.wind.speed} m/s</span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        )}
       </div>
     </div>
   );
